e81677ffc9b21b2d4eda7a4407330ba8d076ea64
Vladislav Kuznetsov <gloomy.fox123@gmail.com>
2024-06-20 22:27:54 +0300

    lesson 7. Generics

---
+++ lesson 7. Generics/app.js
@@ -0,0 +1,34 @@
+"use strict";
+/*
+
+Задание:
+
+Необходимо написать функцию сортировки любых
+объектов, которые имеют id по убыванию и по возрастанию
+
+Исходные данные:
+
+const data = [
+  { id: 2, name: 'Петя' },
+  { id: 1, name: 'Вася' },
+  { id: 3, name: 'Надя' },
+];
+
+*/
+const data = [
+    { id: 2, name: 'Петя' },
+    { id: 1, name: 'Вася' },
+    { id: 3, name: 'Надя' },
+];
+function getSortedArr(arr, sortType = "toRise" /* riseOrDown.toRise */) {
+    return arr.sort((a, b) => {
+        switch (sortType) {
+            case "toRise" /* riseOrDown.toRise */:
+                return a.id - b.id;
+            case "toDown" /* riseOrDown.toDown */:
+                return b.id - a.id;
+        }
+    });
+}
+console.log(getSortedArr(data, "toRise" /* riseOrDown.toRise */)); // сортировка по возрастанию id
+console.log(getSortedArr(data, "toDown" /* riseOrDown.toDown */)); // сортировка по убыванию id
